5732b9f5ded05991417653ecd257dbd6fe8d9a41
jonathan-grumke <grumkejonathan@yahoo.de>
2025-03-17 21:15:22 +0700

    add posts component

---
+++ src/components/BlogPosts.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { getCollection } from "astro:content";
+import FormattedDate from "./FormattedDate.astro";
+import "./BlogPosts.css"
+
+const BlogPosts = async ({ collection, numberOfPosts }) => {
+    const posts = (await getCollection(collection)).sort(
+        (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
+    ).slice(0, numberOfPosts);
+
+    return (
+        <>
+            <section>
+                <h3 style={{ marginBottom: "20px" }}>Du hast unsere letzten Weekly Updates noch nicht gelesen?</h3>
+                <ul>
+                    {
+                        posts.map((post) => (
+                            <li>
+                                <a href={`/blog/${post.id}/`}>
+                                    <img
+                                        width={720}
+                                        height={360}
+                                        src={post.data.heroImage}
+                                        alt=""
+                                    />
+                                    <h4 class="title">{post.data.title}</h4>
+                                </a>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </section>
+        </>
+    )
+
+
+}
+
+export default BlogPosts;
