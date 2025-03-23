83d92ad8e6e15937d6f9ab0844417ec4ac008ac2
jonathan-grumke <grumkejonathan@yahoo.de>
2025-03-23 07:49:23 +0700

    update meta descriptions and titles

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Sun in my pocket';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Sun in my Pocket";
+export const SITE_DESCRIPTION =
+    "Lies über unsere Abenteuer auf der ganzen Welt und finde Reiserouten, Städtetrips, Tipps und Empfehlungen für Reiseziele rund um den Globus. Reise mit uns!";
